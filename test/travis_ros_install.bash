08b3304620da7241d936acd22a663ed68b00b7a8
Duca1100 <cooper@tokyo.email.ne.jp>
2017-05-17 02:13:36 +0000

    Add Travis CI

--- test/travis_ros_install.bash
+++ test/travis_ros_install.bash
@@ -9,15 +9,15 @@ pip install rospkg
 #ros install
 #cd ..
 #git clone https://github.com/Duca1100/ros_setup_scripts_Ubuntu14.04_server.git
-cd /home/ros_setup_scripts_Ubutu14.04_server
-bash ./step0.bash
-bash ./step1.bash
+#cd /home/ros_setup_scripts_Ubutu14.04_server
+#bash ./step0.bash
+#bash ./step1.bash
 
 #catkin setup
-mkdir -p ~/catkin_ws/src
-cd ~/catkin_ws/src
-source /opt/ros/indigo/setup.bash
-catkin_init_workspace
-cd ~/catkin_ws
-catkin_make
+#mkdir -p ~/catkin_ws/src
+#cd ~/catkin_ws/src
+#source /opt/ros/indigo/setup.bash
+#catkin_init_workspace
+#cd ~/catkin_ws
+#catkin_make
 
