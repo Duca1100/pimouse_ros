8e0b57b37372320c0a9bf8dd5d4e9a81e01a10f3
Duca1100 <cooper@tokyo.email.ne.jp>
2017-05-17 03:07:20 +0000

    Add Travis CI

--- test/travis_ros_install.bash
+++ test/travis_ros_install.bash
@@ -14,10 +14,10 @@ bash ./step0.bash
 bash ./step1.bash
 
 #catkin setup
-#mkdir -p ~/catkin_ws/src
-#cd ~/catkin_ws/src
-#source /opt/ros/indigo/setup.bash
-#catkin_init_workspace
-#cd ~/catkin_ws
-#catkin_make
+mkdir -p ~/catkin_ws/src
+cd ~/catkin_ws/src
+source /opt/ros/indigo/setup.bash
+catkin_init_workspace
+cd ~/catkin_ws
+catkin_make
 
